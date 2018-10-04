c22dcdb1a529cebf87a041d794798e49c474b66c
Spazcool <douglas.wrightiii@gmail.com>
2018-10-03 22:08:43 -0400

    change to Function instead of eval

--- checkAnswer.js
+++ checkAnswer.js
@@ -1,7 +1,13 @@
 function checkAnswer(answer, question){
   // console.log("a: ", answer, " q: ", question);
   // console.log("q eval: ", eval(question));
-  if(answer == eval(question)){
+  // I DOUBT WE CAN RELY ON THE EVAL FUNCTION FOR MORE COMPLEX MATH, ESPECIALLY WHEN SPECIAL CHARS ARE BEING USED
+  // TODO
+  // FIND EQUIVALENT TO EVAL FOR COMPLEX MATH CHARS, POSSIBLY A LIBRARY SOLUTION
+  // Function('"use strict";return (' + question + ')')();
+
+  // if(answer == eval(question)){
+  if(answer == Function('"use strict";return (' + question + ')')()){
     return true;
   }
   return false;
